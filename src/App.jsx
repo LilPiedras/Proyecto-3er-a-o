1ac91d2dc533b1aee6d680ffbb477f8017f6daa3
LilPiedras <bigfeetgirl01@gmail.com>
2026-03-13 13:19:07 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
+import { Routes, Route } from "react-router-dom"
 import Fromc from "./components/From"
+import Fromt from "./components/Fromr"
 import Footers from "./components/Footer"
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen w-full bg-white">
-      <div className="flex flex-1 w-full overflow-hidden">
-        
-        <div className="w-full lg:w-1/2 flex items-center justify-center p-4">
-          <Fromc />
-        </div>
-        <div className="hidden relative lg:flex lg:w-1/2 items-center justify-center bg-linear-to-tr from-gold-light via-gold-dark to-black-rich overflow-hidden">
-          <div className="relative w-full h-full flex items-center justify-center">
-            <div className="w-60 h-60 bg-[url('/img/Logo.png')] bg-cover rounded-full animate-bounce duration-10 shadow-xl"  />  
-            <div>
+    <>
+      <Routes>
+
+        <Route path="/" element={
+          <div className="min-h-screen flex flex-col lg:flex-row">
+            <div className="w-full lg:w-1/2 flex items-center justify-center p-4 bg-black">
+              <Fromc />
+            </div>
+            <div className="hidden relative lg:flex lg:w-1/2 items-center justify-center bg-linear-to-tr from-yellow-200 via-yellow-500 to-black overflow-hidden">
+              <div className="relative w-full h-full flex items-center justify-center">
+                <div className="w-60 h-60 bg-[url('/img/Logo.png')] bg-cover rounded-full animate-bounce shadow-xl" />
+              </div>
             </div>
           </div>
-          
-        </div>
-      </div>
+        } />
 
-      {/* Footer */}
+        <Route path="/registro" element={
+          <div className="min-h-screen flex flex-col lg:flex-row">
+            <div className="w-full lg:w-1/2 flex items-center justify-center p-4 bg-black">
+              <Fromt />
+            </div>
+            <div className="hidden relative lg:flex lg:w-1/2 items-center justify-center bg-linear-to-tr from-yellow-200 via-yellow-500 to-black overflow-hidden">
+              <div className="relative w-full h-full flex items-center justify-center">
+                <div className="w-60 h-60 bg-[url('/img/Logo.png')] bg-cover rounded-full animate-bounce shadow-xl" />
+              </div>
+            </div>
+          </div>
+        } />
+      </Routes>
       <Footers />
-      
-    </div>
+    </>
   )
 }
 
